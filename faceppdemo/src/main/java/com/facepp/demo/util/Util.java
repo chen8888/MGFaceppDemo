23f054a626a988af59c8138b1fb3a00efdc7b9b1
chenjingwu@sightp.com <Abc12345>
2019-12-23 19:28:07 +0800

    111

---
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "2lcW8Rrw3w4FxtYT2Iz81pSblR0fNOwz";
+	public static String API_SECRET = "DDcKL80IchAPPdxpdAbkxwGolp4TXOQe";
+
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
+}
